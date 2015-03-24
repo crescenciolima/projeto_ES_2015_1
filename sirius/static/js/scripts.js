c37721d3b033207be78e8c267cf81dbe78307ff3
kauannavs <kauannavs@gmail.com>
2015-03-24 19:07:18 -0300

    pasta static

---
+++ sirius/static/js/scripts.js
@@ -0,0 +1,10 @@
+/**
+ * Created by joabe_000 on 26/02/2015.
+ */
+
+// A $( document ).ready() block.
+$( document ).ready(function() {
+    $.vegas('overlay', {
+      src:'http://127.0.0.1:8000/static/img/overlays/01-clean.png'
+    });
+});
--- sirius/views.py
+++ sirius/views.py
@@ -1,1 +1,16 @@
-# Create your views here.
+from django.shortcuts import render, render_to_response, get_object_or_404
+from django.template import RequestContext
+from django.http import HttpResponseRedirect, HttpResponse
+from models import ModeloArquitetura,ModeloArquiteturaAvaliacao,Tecnologias,TradeOff,Diretriz,Referencia
+from forms import FormModeloArquitetura
+
+def index(request):
+    if request.method == 'POST':
+        form = FormModeloArquitetura(request.POST, request.FILES)
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.save()
+            return render(request,"aviso_salvo",{})
+    else:
+        form = FormModeloArquitetura()
+    return render(request,"",{"form":form},context_instance=RequestContext(request))

e1f5e26f576acc48775052d6682cb7cec6ccd749
igoromero <igo_2007@hotmail.com>
2015-03-25 23:43:37 -0300

    Alteraçao no Arquivo Models.py    -> Alteração na estrutura da class AtributoQualidade     ->  Inserção medoto def __unicode__(self): nas classes

--- canary/models.py
+++ canary/models.py
@@ -16,11 +16,16 @@ class Autores(models.Model):
     projeto = models.OneToOneField(Projeto)
     nome = models.CharField(max_length=90)
 
+    def __unicode__(self):
+        return '%s' % self.nome
 
 class Introducao(models.Model):
     projeto = models.OneToOneField(Projeto, primary_key=True)
     introducao = models.TextField()
     objetivo = models.TextField()
+
+    def __unicode__(self):
+        return '%s %s' % self.introducao, self.objetivo
 
 class Referencia(models.Model):
     titulo = models.CharField(max_length=90)
@@ -28,12 +33,16 @@ class Referencia(models.Model):
     descricao = models.TextField()
     projeto = models.ManyToManyField(Projeto)
 
+    def __unicode__(self):
+        return '%s, %s, %s' % self.titulo, self.autores, self.descricao
 
 class Tecnologia(models.Model):
     api = models.ManyToManyField(API)
     razaoUso = models.CharField(max_length=100)
     projeto = models.ManyToManyField(Projeto)
 
+    def __unicode__(self):
+        return '%s , %s, %s' % self.api, self.razaoUso, self.projeto
 
 class API(models.Model):
     tecnologia = models.OneToOneField(Projeto, primary_key=True)
@@ -41,15 +50,33 @@ class API(models.Model):
     versao = models.CharField(max_length=10)
     especificacao = models.TextField()
 
+    def __unicode__(self):
+        return '%s, %s, %s' % self.nome, self.versao, self.especificacao
+
+classificacao = (
+                    ('0', '0'),
+                    ('1', '1'),
+                    ('2', '2'),
+                    ('3', '3'),
+                    ('4', '4')
+)
+
 class AtributodeQualidade(models.Model):
     projeto = models.ForeignKey(Projeto)
-    nome = models.CharField(max_length=80)
-    prioridade = models.IntegerField
+    funcionamento = models.CharField(max_length=2, choices=classificacao)
+    confiabilidade = models.CharField(max_length=2, choices=classificacao)
+    usabilidade = models.CharField(max_length=2, choices=classificacao)
+    eficiencia = models.CharField(max_length=2, choices=classificacao)
+    manutenibilidade = models.CharField(max_length=2, choices=classificacao)
+    portabilidade = models.CharField(max_length=2, choices=classificacao)
 
 class Feature(models.Model):
     projeto = models.OneToOneField(Projeto, primary_key=True)
     nome = models.CharField(max_length=50)
     descricao = models.TextField()
+
+     def __unicode__(self):
+        return '%s, %s' % self.nome, self.descricao
 
 class NaoFuncional(models.Model.Feature):
     fonte = models.CharField(300)
@@ -59,5 +86,13 @@ class NaoFuncional(models.Model.Feature):
     resposta = models.CharField(300)
     medicao = models.CharField(300)
 
+     def __unicode__(self):
+        return '%s, %s, %s, %s, %s, %s' % self.fonte, self.estimulo, self.ambiente, self.artefato, self.resposta, self.medicao
+
+
 class Funcional(models.Model.Feature):
-    nome1 = models.CharField()
+    nome = models.CharField()
+    descricao = models.CharField()
+
+    def __unicode__(self):
+        return '%s, %s' % self.nome, self.descricao

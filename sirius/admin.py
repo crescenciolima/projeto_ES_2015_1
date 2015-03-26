def473f0fb3634c4cf900b2c9e5d7148223db72e
kauannavs <kauannavs@gmail.com>
2015-03-26 08:16:35 -0300

    teste admin

--- sirius/admin.py
+++ sirius/admin.py
@@ -1,1 +1,19 @@
+from sirius.models import ModeloArquitetura,ModeloArquiteturaAvaliacao,Tecnologias,Referencia,TradeOff,Diretriz
+from django.contrib import admin
 
+class TecnologiaInline(admin.TabularInline):
+    model = Tecnologias
+    extra = 1
+
+class ReferenciaInline(admin.TabularInline):
+    model = Referencia
+    extra = 0
+
+class ModeloArquiteturaAdmin(admin.ModelAdmin):
+    model = ModeloArquitetura
+    list_display = ['nome','introducao']
+    search_fildes = ['nome']
+    inlines = [TecnologiaInline, ReferenciaInline]
+    save_on_top =True
+
+admin.site.register(ModeloArquitetura, ModeloArquiteturaAdmin)

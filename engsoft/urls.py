62e2121f2d4fa7baaed6b1d1f0893f05c07a920a
kauannavs <kauannavs@gmail.com>
2015-04-09 00:14:43 -0300

    Atualizando a visualização das imagens no admin

--- engsoft/urls.py
+++ engsoft/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -12,6 +13,9 @@ urlpatterns = patterns('',
 
     # Uncomment the admin/doc line below to enable admin documentation:
     url(r'^admin/destino/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/destino(?P<path>.*)$', 'django.views.static.serve', {
+            'document_root': settings.MEDIA_ROOT,
+        }),
 
     #url(r'^view_imagem/', 'sirius.views.view_imagem'),
     # Uncomment the next line to enable the admin:
